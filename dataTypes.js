c489f6ce8fa8eb37eb9e5959f956a860c53ccc41
leonischu <nischal.201423@ncit.edu.np>
2026-01-13 11:03:52 +0545

    DataTypes

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3)//we are using nodeJs ,not browser 
+console.log(3+3);
+
+let name = "Nischal";
+let age = 18;
+let isLoggedIn = false ;
+
+//symbol => unique
+
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined 
+
+
---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let acountEmail = "Nischal@gmail.com"
+var accountPassword  = "12345"
+accountCity = "Nepal"
+
+accountEmail = "hchc@hc.com"
+accountPassword = "3232232";
+accountCity= "kathmandu"
+
+console.table([accountId,acountEmail,accountPassword,accountCity]);
+console.log(accountId);
