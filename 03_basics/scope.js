1c8446f5df6d31d2da8d7216526301b96b967ac8
leonischu <nischal.201423@ncit.edu.np>
2026-01-14 10:17:31 +0545

    scope level

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,9 +1,49 @@
+// const { use } = require("react");
 
-let a
+// let a
 
-if (true)
-{
-    // var c = 30
+// if (true)
+// {
+//     // var c = 30
+// }
+
+// console.log(c)
+
+// function one(){
+//     const username = "Nischal"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two();
+// }
+// one();
+
+
+if(true){
+    const username = "Nischal"
+    if(username === "Nischal"){
+        const website = "  youtube"
+        // console.log(username + website)
+    }
+    // console.log(website);
 }
+// console.log(username);
 
-console.log(c)
+
+
+
+//+++++++++++++++++++++++Interesting+++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+addone(5);
+const addTwo = function(num){
+
+return num + 2
+
+
+}
+addTwo(5)
