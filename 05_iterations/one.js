65af94440022d0609043e58277f14adafddaea00
leonischu <nischal.201423@ncit.edu.np>
2026-01-14 12:16:52 +0545

    Iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,33 @@
+// for(let i = 0; i<=10; i++){
+//     // console.log(`outer loop value: ${i}`);
+//     for(let j = 0; j<=10;j++){
+   
+//      // console.log(`Inner loop vale ${j} and inner loop ${i}`);
+
+//         // console.log(i + '*' + j + '=' + i*j)
+
+//     }
+// }
+
+// break and continue
+
+
+// for (let i = 1; i <=20; i++) {
+
+//         if(i == 5){
+//             console.log(`Detected 5`);
+//             break;
+//         }
+
+
+//     console.log(`Value of i is ${i}`);
+// }
+
+
+while (condition) {
+    
+}
+
+do {
+    
+} while (condition);
